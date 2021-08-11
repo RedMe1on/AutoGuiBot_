32c83cb90e28a7f65d02505811e28451c21a9b5a
RedMe1on <lalkogad@gmail.com>
2021-08-11 15:34:14 +0300

    add files to user

---
+++ competition.py
@@ -0,0 +1,5 @@
+from app import AutoRequestArrayCompetition
+
+if __name__ == '__main__':
+    c = AutoRequestArrayCompetition()
+    c.request_many_competition()
---
+++ registration.py
@@ -0,0 +1,5 @@
+from app import AutoRegistrationArrayAccount
+
+if __name__ == '__main__':
+    c = AutoRegistrationArrayAccount()
+    c.registration_many_accounts()
